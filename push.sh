b6d156b6d226414251257470aa9b0c8480a80981
dekuNukem <dekunukem@gmail.com>
2026-03-21 00:03:03 +0000

    new branch

--- push.sh
+++ push.sh
@@ -1,15 +1,28 @@
-if [[ $# -eq 0 ]] ; then
-    echo 'error: please enter a commit comment'
-    exit 1
+# If no arguments are provided, ask for a comment
+if [[ $# -eq 0 ]]; then
+    read -p "Enter a commit message: " commit_msg
+    if [[ -z "$commit_msg" ]]; then
+        echo "error: commit message cannot be empty"
+        exit 1
+    fi
+else
+    commit_msg="$*"
 fi
 
+# Cleanup temp and cache files
+find . -type f -name "*.DS_Store*" -exec rm -f {} \;
 find . -type f -name "*.b#*" -exec rm -f {} \;
 find . -type f -name "*.s#*" -exec rm -f {} \;
 find . -type f -name "*.l#*" -exec rm -f {} \;
 find . -type f -name "*.csv#*" -exec rm -f {} \;
-find . -type f -name "*.DS_Store*" -exec rm -f {} \;
 find . -name "__pycache__" -exec rm -rf {} \;
 
 git add --all
-git commit -m "$@"
-git push origin master
+
+# Get current branch
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+# Stage, commit, and push
+git add --all
+git commit -m "$commit_msg"
+git push origin "$current_branch"
